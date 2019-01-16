2ff88fe027f6fd2a78ad9eedba7fc4749da6ea00
Paul Lynn <lynnporu@gmail.com>
2019-01-16 09:09:44 +0200

    Add strproc library; tokenize function

---
+++ libs/strproc.py
@@ -0,0 +1,45 @@
+import re
+
+
+REDASHSET = r'[\u2012-\u2015\u002D]'
+REDASH = r'\u2012\u2013\u2014\u2015\u002D'
+REELLIPSIS = r'[\u2026\u22EF\u1801]|(?:\.)(?!\.)|(?:\.\.)(?!\.)|(?:\.\.\.)'
+# ~@?!&^*, symbols
+REMARKS = r'[\u007E\u0040\u003F\u0021\u0026\u005E\u002A\u002C]'
+REBRACKETS = r'[[\]()\{}]'
+RECOLONS = r'[:;]'
+# Quotation marks
+REQUOT = r'[\u2018\u2019\u201C\u201D\u0022]'
+RESLASH = r'[\u002F\u005C\u007C\u00A6]'
+REPUNCT = (
+    rf'{REDASHSET}|{REELLIPSIS}|{REMARKS}|{REQUOT}|{RECOLONS}|{RESLASH}|'
+    rf'{REBRACKETS}'
+)
+RESPACE = r'[\u2003\u2002\u0020]'
+# List of symbols that often occurs near the decimals (e.g "±5%"")
+RENUM = r'[\u2213\u00B1\u002B\u002B\u0025\u2031\u00B0\u0023\u2116\u00A7]'
+REMATH = r'[\u002B\u005C\u002D\u00D7\u00F7\u2213\u00B1]'
+# Symbols that can separate two numbers: "5..8", "5/8", "5-8"
+RENUMSEP = rf'{REELLIPSIS}|{REDASHSET}|{RECOLONS}|{RESLASH}|{REMATH}'
+RECURRENCY = r'[\u20A0-\u20CF\u00A2-\u00A5\u0024]'
+# List of all ukrainian symbols
+RECYRRUA = r'[А-ЩЬЮ-щьюяіїґєІЇҐЄ]'
+
+
+def tokenize(sentence):
+    """Tokenize the given sentence.
+
+    Args:
+        sentence (str): String that must be processed.
+
+    Returns:
+        list: List of tokens.
+
+    """
+    retokens = re.compile(
+        fr'(?:\d+{RENUMSEP}\d+)'
+        fr'|(?:{RENUM}*{RECURRENCY}*\d+{RECURRENCY}*{RENUM}*)|'
+        fr'{REPUNCT}|[\w{REDASH}\u0301]+'
+    )
+
+    return retokens.findall(sentence)
