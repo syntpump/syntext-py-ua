f7395364e3a2ef971b4b557d36ed0508126377e8
Paul Lynn <lynnporu@gmail.com>
2019-02-07 00:37:46 +0200

    Token processing in HumanTrainer
    Try to run with this:
    xpostrain --path ... -unstrict --reader conllu.ConlluReader --trainer human.HumanTrainer --entry nextXPOS --limit 100 --rulescollection ... --applierFunction libs.morphology.MorphologyRecognizer.selectByEnding --priorityList ...

--- libs/tmr/human.py
+++ libs/tmr/human.py
@@ -3,12 +3,24 @@
 
 from ..morphtrain import MorphologyRecognizeTrainer
 
-from ..morphology import MorphologyRecognizer
+from libs.morphology import MorphologyRecognizer
 from importlib import import_module
 from libs.arrproc import unduplicate, keyExtract
-
+from libs.ui import expect
+from pprint import pprint
+import json
 
 class HumanTrainer(MorphologyRecognizeTrainer):
+    """This trainer can analyze errors in recognition and allow use to create
+    new rules.
+
+    Properties:
+        rules (list): List of data of rules that was inputed by user before.
+            This will be used for suggestions.
+
+    """
+       
+    rules = list()
 
     def nextXPOS(self):
         """Generator function. Each next iteration process one POS from
@@ -20,8 +32,10 @@ class HumanTrainer(MorphologyRecognizeTrainer):
                 which contains rules to test.
             applierFunction (str): Name of applier function for morphology
                 recognizer.
+            priorityList (str): Priority list for recognizer. (optional)
 
         """
+
 
         applierAddr = self.settings["applierFunction"].split(".")
         # First part of address is a module
@@ -40,30 +54,120 @@ class HumanTrainer(MorphologyRecognizeTrainer):
             )
         )
 
+        priorityList = json.loads(self.settings["priorityList"])
+
         # Iterate each POS
         for upos, xpos in self.poses:
 
-            tokens = unduplicate(
-                keyExtract(
-                    list(
-                        self.tempcoll.find({
-                            "xpos": xpos
-                        })
-                    ),
-                    "form"
-                )
-            )
+            # Repeat this continuously unless user decide to break it
+            while True:
+                try:
 
-            print(f"Training {upos} {xpos}.")
-            print("Tokens found: %s" % ', '.join(tokens), end="")
+                    # If some mistake in recognition will happen, this flag
+                    # will be set to True, so user will be suggested to repeat
+                    # processing this POS
+                    mistakeExists = False
 
-            # Iterate each token of this POS
-            for token in tokens:
-                    recoResponse = recognizer.recognize(
-                        token, applier
+                    tokens = unduplicate(
+                        keyExtract(
+                            list(
+                                self.tempcoll.find({
+                                    "xpos": xpos
+                                })
+                            ),
+                            "form"
+                        )
                     )
 
-                    print(f"For \"{token}\" response:")
-                    print(recoResponse)
+                    print(f"Training {upos} {xpos}.")
+                    print("Tokens found: %s.\n" % ', '.join(tokens))
+
+                    # Iterate each token of this POS
+                    for token in tokens:
+
+                            print(f"Current token is: {token}")
+
+                            recogn = recognizer.recognize(
+                                token, applier
+                            )
+
+                            result = recognizer.recognize(token)
+                            appRes = recognizer.recognize(
+                                token, applier, priorityList
+                            )
+
+                            print(
+                                f"Recognized as {appRes['upos']} "
+                                f"{appRes['xpos']}."
+                            )
+
+                            if xpos == appRes["xpos"]:
+                                ruleType = (
+                                    recogn['type']
+                                    if recogn['type'] else "<unknown>"
+                                )
+                                print(
+                                    "Recognized correctly. "
+                                    f"Type of rule: {ruleType}\n"
+                                )
+                                continue
+                            else:
+                                mistakeExists = True
+
+                            if xpos in keyExtract(result, "xpos"):
+                                print(
+                                    "Recognizing of this token may be "
+                                    "improved. Here's the DB output:"
+                                )
+                            else:
+                                print(
+                                    "Recognized incorrectly. Here's the "
+                                    "DB output:"
+                                )
+                            pprint(result, indent=4, compact=True)
+
+                            decision = expect(
+                                msg=(
+                                    "Skip this token or make a new rule? "
+                                    "(s - Skip|r - Rule): "
+                                ),
+                                what = ["r", "s"]
+                            )
+
+                            if decision == "s":
+                                continue
+                            elif decision == "r":
+                                self.newRule()
+
+                    if not mistakeExists:
+                        raise ContinueException
+
+                    if (
+                        expect(
+                            msg="Repeat processing this POS? (y|n): ",
+                            what=["y", "n"]
+                        ) == "y"
+                    ):
+                        print("\n\n")
+                        raise RepeatException
+                    break
+
+                except RepeatException:
+                    continue
+
+                except ContinueException:
+                    break
 
             yield "\n\n"
+
+    def newRule(self):
+        """Show the UI to create new rule for DB and adds in to DB.
+        """
+
+        print("*pretends to be rule UI*")
+
+class RepeatException(Exception):
+    pass
+
+class ContinueException(Exception):
+    pass
