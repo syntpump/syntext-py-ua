8343f7fa877cd727e14930165443599cc56e7266
Paul Lynn <lynnporu@gmail.com>
2019-04-08 22:50:25 +0300

    Allow initializing for any class
    Nested class will be initialized recursively

--- pysyntext/predefinator.py
+++ pysyntext/predefinator.py
@@ -7,6 +7,7 @@
 
 import json
 from importlib import import_module
+import os
 import pymongo
 from libs.db import DB
 from libs.morphology import MorphologyRecognizer
@@ -33,16 +34,16 @@ class Predefinator:
         cp_config = config["ContextualProcessor"]
         cr_config = config["ConlluReader"]
 
-    def getClass(self, path):
-        """Return class by its path.
+    def getByPath(self, path):
+        """Returns object by its path.
 
         Properties:
-            path (str): Path to class.
+            path (str): Path to object.
 
         Returns:
-            uninitialized class
+            uninitialized class, function
 
-        Throws:
+        Raises:
             AttributeError: Given path does not exist.
 
         """
@@ -54,6 +55,62 @@ class Predefinator:
             obj = getattr(obj, path.pop(0))
 
         return obj
+
+    def inited(self, name=None, properties=None, location=None):
+        """Returns well-initialized class using configs from self.config.
+
+        Args:
+            name (str): Name of class to initialize
+            properties (dict): Properties for class to initialize. If not
+                passed, then self.config will be used.
+            location (str): Location where to look for class. If not passed
+                then self.config will be used.
+
+        Returns:
+            initialized class
+
+        Raises:
+            KeyError: Given class does not exist in config file.
+
+        """
+
+        if not name and not location:
+            raise TypeError("Name of class or its location must be given.")
+
+        if name:
+            location = self.config[name]["$location"] + "." + name
+
+        obj = self.getByPath(location)
+        initprops = {}
+
+        if not properties:
+            properties = self.config[name]
+
+        for prop, value in properties.items():
+
+            if prop[0] == "$":
+                continue
+
+            if type(value) is not dict:
+                initprops[prop] = value
+            else:
+                if value["object"] == "function":
+                    initprops[prop] = self.getByPath(value["name"])
+                elif value["object"] == "class":
+                    # Default properties from config.json must be used
+                    if "props" not in value:
+                        initprops[prop] = self.inited(value["name"])
+                    # Special properties are defined
+                    else:
+                        initprops[prop] = self.inited(
+                            properties=value["props"],
+                            location=value["name"]
+                        )
+                elif value["object"] == "sysvar":
+                    initprops[prop] = os.getenv(value["name"])
+
+        # Unpack dictionary with properties and initialize obj, which is class
+        return obj(**initprops)
 
     def defineDB(self):
         """Returns predefined DB object
