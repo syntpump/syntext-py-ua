669873458d57d7c8fbc08413c6243f97dc41aa2a
Paul Lynn <lynnporu@gmail.com>
2019-04-17 11:21:52 +0300

    Add tagparser parametrizing in xpostrain

--- pysyntext/xpostrain.py
+++ pysyntext/xpostrain.py
@@ -28,6 +28,8 @@ Name            Default     Description
 --limit ...     0           Limit of tokens to be processed. Can be used for
                             testing script. Pass '0' to set it to infinite.
 --offset ...    0           Skip first N tokens from GC.
+--useUDT        False       Determine whether to use UDT in ConlluReader or not.
+--tagparser     (optional)  Name of a tagparser class if your trainer need it.
 --confs         config.json Address to file with configurations.
 ...Plus additional parameters needed for the trainer you chose.
 """ # noqa E122
@@ -54,6 +56,9 @@ db = DB(
     host=argv.get("--dbhost", default="atlas"),
     dbname="syntextua"
 )
+
+if argv.has("--tagparser"):
+    argv.bundle["--tagparser"] = predef.inited(argv.get("--tagparser"))
 
 trainer = predef.inited(
     argv.get("--trainer"),
