614c91ac76195a7ce6b3c030ee23b77a04fc8bc0
Alex <mrtoadstooler@gmail.com>
2019-05-25 20:22:40 +0300

    treefy function

--- pysyntext/cykalgo.py
+++ pysyntext/cykalgo.py
@@ -1,6 +1,14 @@
 """Contains a class implementing the CYK algorithm.
 """
 
+from pprint import pprint as pp
+
+
+class CYKProcessingError(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
 
 class CYKAnalyzer:
     """Class that uses CYK algorithm to parse sentences with the help of
@@ -44,7 +52,7 @@ class CYKAnalyzer:
                 for j in range(numtokens + 1)]
 
         for i in range(numtokens):
-            wfst[i][i + 1].append(tokens[i])
+            wfst[i][i + 1].append({'pos': tokens[i], 'linksTo': [None, None]})
 
         numtokens += 1
 
@@ -55,14 +63,15 @@ class CYKAnalyzer:
 
                     possible_productions = [
                         (
-                            a['PunctType'] if 'PunctType' in a else a['upos'],
-                            b['PunctType'] if 'PunctType' in b else b['upos']
+                            a['pos']['PunctType'] if 'PunctType' in a['pos'] else a['pos']['upos'],
+                            b['pos']['PunctType'] if 'PunctType' in b['pos'] else b['pos']['upos']
                         ) for a in wfst[start][mid] for b in wfst[mid][end]
                     ]
 
                     for rule in self.grammar:
                         if rule['prod'] in possible_productions:
-                            wfst[start][end].append(rule)
+                            wfst[start][end].append(
+                                {'pos': rule, 'linksTo': [wfst[start][mid], wfst[mid][end]]})
 
         return wfst
 
@@ -84,10 +93,47 @@ class CYKAnalyzer:
             for j in range(1, len(wfst)):
                 print(
                     "%-5s" % (
-                        wfst[i][j][0]['upos']
+                        wfst[i][j][0]['pos']['upos']
                         if wfst[i][j]
                         else '.'),
                     end=''
                 )
             print()
 
+    def treefy(self, wfst):
+
+        if len(wfst[0][len(wfst) - 1]) < 1:
+            raise CYKProcessingError(
+                "The sentence hasn't been processed completely")
+
+        output = []
+        buf = []
+        buf.append(wfst[0][len(wfst) - 1][0])
+        count = 1
+        nextCount = 0
+
+        index = 0
+
+        while count > 0:
+
+            node = buf.pop(0)
+
+            if node:
+                output.append({'id': index, 'word': node['pos']['word'], 'tag': 'T', 'morph': node['pos']} if 'word' in node['pos'] else {
+                              'id': index, 'tag': node['pos']['upos'], 'linksTo': [2 * index + 1, 2 * index + 2]})
+                count -= 1
+                index += 1
+
+            if node and node['linksTo'][0]:
+                buf.append(node['linksTo'][0][0])
+                nextCount += 1
+
+            if node and node['linksTo'][1]:
+                buf.append(node['linksTo'][1][0])
+                nextCount += 1
+
+            if count == 0:
+                count = nextCount
+                nextCount = 0
+
+        pp(output)
